880dc40f89390c3936ffb038667ef05acad4b370
GABRYEL RODRIGUES DOS SANTOS <gabryelsantos346@gmail.com>
2026-03-10 10:00:31 -0300

    Requisição estruturada

---
+++ js/Script.js
@@ -0,0 +1,20 @@
+
+function buscarApi(){
+
+const urlApi = ""//URL da api
+const resposta = fetch(urlApi)
+
+if(resposta.status == 200){
+    
+    //Estruturação de código
+   console.log("Api encontrada")
+                                            //Verificação de resposta da api
+}else{
+    console.log("Api não encontrada")
+}
+
+
+
+}
+
+
