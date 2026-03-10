0b74870bb648f83e659d17eb5447a72b09ade7b6
GABRYEL RODRIGUES DOS SANTOS <gabryelsantos346@gmail.com>
2026-03-10 10:21:25 -0300

    iniciação de requisição

--- js/Script.js
+++ js/Script.js
@@ -1,19 +1,41 @@
 
 function buscarApi(){
 
-const urlApi = ""//URL da api
+const urlApi = "https://unpkg.com/leaflet/dist/leaflet.js"//URL da api
 const resposta = fetch(urlApi)
 
 if(resposta.status == 200){
     
     //Estruturação de código
+
+    let mapa = L.map('mapa').setView([-23.5489, -46.6388], 13);
+
+    L.titleLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+    var clique
+
+
+    mapa.on('click', function(e) {
+        var latitude = e.latlng.lattitude;      //Latitude e longitude para adicionar marcador no mapa
+        var longitude = e.latlng.longitude;
+    });
+
+    if(clique == true){
+        mapa.removeLayer(clique);    //Verificação de clique no mapa para adicionar marcador
+    }
+
+
+    clique = L.marker([latitude, longitude]).addTo(mapa);
+
+
    console.log("Api encontrada")
                                             //Verificação de resposta da api
 }else{
     console.log("Api não encontrada")
 }
-
-
 
 }
 
